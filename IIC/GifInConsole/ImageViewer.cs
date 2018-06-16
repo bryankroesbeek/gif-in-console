13402e945348a730fc9470e63a971a2b010b5417
bryankroesbeek <bryank.b96@gmail.com>
2018-06-16 17:15:21 +0200

    I can now view an image in a small window :)

---
+++ IIC/GifInConsole/ImageViewer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GifInConsole
+{
+    class ImageViewer
+    {
+        private Image Image { get; set; }
+
+        public ImageViewer(Image i)
+        {
+            this.Image = i;
+        }
+
+        public void View()
+        {
+            Form window = new Form();
+            window.Controls.Add(new PictureBox
+            {
+                Image = this.Image,
+                Dock = DockStyle.Fill
+            });
+            Application.Run(window);
+        }
+    }
+}
--- IIC/GifInConsole/Program.cs
+++ IIC/GifInConsole/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+
 
 namespace GifInConsole
 {
@@ -10,6 +13,15 @@ namespace GifInConsole
     {
         static void Main(string[] args)
         {
+            if (args.Length != 1) return;
+
+            // This should be the pathname of the gif that one wants to view in the console
+            string path = args[0];
+
+            Image image = Image.FromFile(path);
+            FrameDimension dimension = new FrameDimension(image.FrameDimensionsList.First());
+            var count = image.GetFrameCount(dimension);
+            new ImageViewer(image).View();
         }
     }
 }
