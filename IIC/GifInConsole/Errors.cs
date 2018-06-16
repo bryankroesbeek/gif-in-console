227fa6ef9740cf49367cbc3636df3d9f11e11687
bryankroesbeek <bryank.b96@gmail.com>
2018-06-16 20:29:59 +0200

    I might have had a little bit to much fun with the errors messages

---
+++ IIC/GifInConsole/Errors.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace GifInConsole
+{
+    class Errors
+    {
+        public static void DisplayReaction(int numberOfErrors)
+        {
+            if (numberOfErrors < 2) Console.WriteLine("NICE TRY YOU DICK");
+            else if (numberOfErrors < 9) Console.WriteLine("...");
+            else if (numberOfErrors < 10) Console.WriteLine("You sure love to try and break this program, don't you?");
+            else if (numberOfErrors < 19) Console.WriteLine("Ugh...");
+            else if (numberOfErrors < 20) Console.WriteLine("WHY CAN'T YOU BE NORMAL");
+            else if (numberOfErrors < 21)
+            {
+                Console.WriteLine("Here, take 15 seconds to decide if you want to do this again\n");
+                for (int i = 0; i < 15; i++)
+                {
+                    Thread.Sleep(1000);
+                    Console.Write(".");
+                }
+            }
+            else if (numberOfErrors < 22)
+            {
+                Console.WriteLine("FUCK YOU, YOU ARE NOT USING THIS PROGRAM ANYMORE");
+                Thread.Sleep(500);
+                Console.WriteLine("fucking dick, i did not create this program for it to be abused by you...");
+                Thread.Sleep(2500);
+                Environment.Exit(0);
+            }
+        }
+    }
+}
