b01aab416130c03c11c803c3e5199004d02a6f85
bryankroesbeek <BryanK.B96@gmail.com>
2018-07-06 13:36:27 +0200

    The project is a little more structured now.

--- IIC/GifInConsole/ImageTools/ImageConverter.cs
+++ IIC/GifInConsole/ImageTools/ImageConverter.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 
-namespace GifInConsole
+namespace GifInConsole.ImageTools
 {
     public static class ImageConverter
     {
--- IIC/GifInConsole/ImageTools/ImageViewer.cs
+++ IIC/GifInConsole/ImageTools/ImageViewer.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace GifInConsole
+namespace GifInConsole.ImageTools
 {
     class ImageViewer
     {
